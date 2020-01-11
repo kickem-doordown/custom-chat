cc555a0836c0ebf30d82396ae122bed78fcb59eb
Kickem Doordown <kickem.doordown@gmail.com>
2020-01-11 12:52:56 -0500

    fix meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private meta: Meta){
+    this.meta.addTag({'http-equiv': 'cache-control', 'content': 'no-cache, must-revalidate, post-check=0, pre-check=0'});
+    this.meta.addTag({'http-equiv': 'expires', 'content': '0'});
+    this.meta.addTag({'pragma': 'cache-control', 'content': 'no-cache'});
+  }
 
 }
