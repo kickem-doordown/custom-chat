b82e95df778ca490603d5e68a17f6390aae609af
Kickem Doordown <kickem.doordown@gmail.com>
2020-01-10 21:23:29 -0500

    change container height on window resize

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnDestroy, OnInit, Renderer2, AfterViewChecked, AfterViewInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { MessagedbService } from '../messagedb.service';
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -10,21 +10,26 @@ import { Router } from '@angular/router';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnDestroy{
+export class ChatComponent implements OnDestroy, AfterViewInit{
   
-  email: string;
   @ViewChild('messageText', {static: false}) 
   messageText: ElementRef;
   @ViewChild('chat', {static: false}) 
   chatContainer: ElementRef;
+  @ViewChild('container', {static: false}) 
+  container: ElementRef;
+
+  email: string;
   messages: Observable<any[]>;
   mesSub: Subscription;
   lastdocid: string;
 
-  constructor(public mesService: MessagedbService, public auth: AngularFireAuth, public router: Router) {
+
+  constructor(public mesService: MessagedbService, public auth: AngularFireAuth, public router: Router, public ren: Renderer2) {
     if(this.auth.auth.currentUser === null){
       this.router.navigate(['/loginpage']);
     } else {
+      
       this.email = this.auth.auth.currentUser.email;
       this.messages = mesService.getObservable();
 
@@ -37,6 +42,12 @@ export class ChatComponent implements OnDestroy{
         this.lastdocid = data[0].docid;
       });
     }
+  }
+  ngAfterViewInit(){
+    this.ren.setStyle(this.container.nativeElement, 'height', window.innerHeight + "px");
+    window.addEventListener("resize", ()=>{
+      this.ren.setStyle(this.container.nativeElement, 'height', window.innerHeight + "px");
+    });
   }
 
   ngOnDestroy(): void {
