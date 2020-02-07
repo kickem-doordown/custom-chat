211d38f95488f8daf2d59a6d1337595aceb9104e
bboy20 <bhumphrey06@gmail.com>
2020-02-06 23:31:11 -0500

    Made message display display name (if there is one)

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -11,16 +11,17 @@ import { AuthService } from '../core/auth.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit, OnDestroy, AfterViewInit{
-  
-  @ViewChild('messageText', {static: false}) 
+export class ChatComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  @ViewChild('messageText', { static: false })
   messageText: ElementRef;
-  @ViewChild('chat', {static: false}) 
+  @ViewChild('chat', { static: false })
   chatContainer: ElementRef;
-  @ViewChild('container', {static: false}) 
+  @ViewChild('container', { static: false })
   container: ElementRef;
 
   email: string;
+  displayName: string;
   messages: Observable<any>;
   mesArr: any[] = [];
   mesSub: Subscription;
@@ -28,26 +29,27 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewInit{
 
   messageNum: number = 0;
   pageSize: number = 10;
-  containerStyle = {"height": window.innerHeight + "px"};
+  containerStyle = { "height": window.innerHeight + "px" };
 
   constructor(public mesService: MessagedbService, public auth: AuthService, public router: Router) {
-    if(localStorage.getItem('user') == null){
+    if (localStorage.getItem('user') == null) {
       this.router.navigate(['/loginpage']);
     } else {
-      
+
       this.email = this.auth.userData.email;
+      console.log(this.auth.userData);
       this.messages = mesService.getRecentMessages(this.pageSize);
       this.messageNum = this.pageSize;
 
       this.messages.pipe(take(1)).subscribe(data => {
         console.log(data);
-        this.mesArr= data.docs;
+        this.mesArr = data.docs;
       });
 
       this.mesSub = mesService.getMessageUpdates().subscribe(data => {
         console.log(data);
-      
-        if(data[0].type === 'added' ) {
+
+        if (data[0].type === 'added') {
           console.log("new message: " + data[0].payload.doc.id);
           this.mesArr.unshift(data[0].payload.doc);
           this.messageNum++;
@@ -56,57 +58,61 @@ export class ChatComponent implements OnInit, OnDestroy, AfterViewInit{
     }
   }
 
-  ngOnInit(){
+  ngOnInit() {
     const tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
     document.body.appendChild(tag);
   }
 
-  ngAfterViewInit(){
-    window.addEventListener("resize", ()=>{
-      this.containerStyle = {"height": window.innerHeight + "px"};
+  ngAfterViewInit() {
+    window.addEventListener("resize", () => {
+      this.containerStyle = { "height": window.innerHeight + "px" };
     });
 
-    this.chatContainer.nativeElement.addEventListener("scroll", ()=>{
-      
-      if( this.chatContainer.nativeElement.scrollTop == 0 && this.mesArr.length == this.messageNum){
+    this.chatContainer.nativeElement.addEventListener("scroll", () => {
+
+      if (this.chatContainer.nativeElement.scrollTop == 0 && this.mesArr.length == this.messageNum) {
         this.messageNum += this.pageSize;
-        this.mesService.getPageAfter(this.pageSize, this.mesArr[this.mesArr.length-1]).subscribe((data)=>{
+        this.mesService.getPageAfter(this.pageSize, this.mesArr[this.mesArr.length - 1]).subscribe((data) => {
           console.log(this.messageNum);
           this.mesArr = this.mesArr.concat(data.docs);
         });
-        console.log( "SCROLL");
+        console.log("SCROLL");
       }
     });
   }
-  
+
   ngOnDestroy(): void {
-    if(this.mesSub)
+    if (this.mesSub)
       this.mesSub.unsubscribe();
   }
 
-  sendMessage(event, buttonType){
+  sendMessage(event, buttonType) {
     console.log(event);
     console.log(buttonType);
-    if(this.messageText.nativeElement.value && this.messageText.nativeElement.value !== ''){
-      this.mesService.sendMessage({user: buttonType === "normal" ? this.email : "[anon]", value: this.messageText.nativeElement.value});
+    if (this.messageText.nativeElement.value && this.messageText.nativeElement.value !== '') {
+      this.mesService.sendMessage({ 
+        user: buttonType === "normal" 
+          ? (this.displayName != null ? this.displayName : this.email) 
+          : "[anon]", 
+        value: this.messageText.nativeElement.value });
       this.messageText.nativeElement.value = "";
     }
     this.messageText.nativeElement.focus();
     event.stopPropagation();
   }
 
-  onMessageLike(messageObj){
+  onMessageLike(messageObj) {
     this.mesService.likeMessage(messageObj, this.email);
-    if(this.messageText.nativeElement.value) {
+    if (this.messageText.nativeElement.value) {
       this.messageText.nativeElement.focus();
     }
   }
 
   scrollToBottom() {
     try {
-        this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
-    } catch(err) { }                 
+      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+    } catch (err) { }
   }
 
 }
