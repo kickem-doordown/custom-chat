bba76f2a2892ae9246aef2be2dd16dce34930ee3
sl4cks <10481627+sl4cks@users.noreply.github.com>
2020-04-26 22:55:09 -0400

    Add copyMessage function

--- src/app/chat-options/chat-options.component.ts
+++ src/app/chat-options/chat-options.component.ts
@@ -51,6 +51,20 @@ export class ChatOptionsComponent implements OnInit {
     return this.window.location.href + "invite/?key=" + this.chatData.inviteLink;
   }
 
+  copyMessage(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
   leaveChat(){
     this.chatdb.leaveCurrentChat();
   }
